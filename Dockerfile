d32383c069ca01db0d720c791fae08570acc3f08
Brian Li <bl@sm.xyz>
2025-06-05 09:11:41 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:24.04
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    build-essential \    
+    curl \
+    git \
+    libclang-dev \
+    libssl-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+RUN rustup default stable
+
+RUN curl -L https://github.com/peak/s5cmd/releases/download/v2.3.0/s5cmd_2.3.0_Linux-64bit.tar.gz -o /tmp/s5cmd.tar.gz
+RUN tar -xzf /tmp/s5cmd.tar.gz -C /usr/local/bin
+RUN rm /tmp/s5cmd.tar.gz
+
+COPY build.sh /app/build.sh
+RUN chmod +x /app/build.sh
+
+# CMD ["bash", "/app/build.sh"]
+CMD ["tail", "-f", "/dev/null"]
---
+++ build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "AWS_ACCESS_KEY_ID is not set"
+    exit 1
+fi
+
+if [ -z "$AWS_ENDPOINT_URL" ]; then
+    echo "AWS_ENDPOINT_URL is not set"
+    exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+    echo "AWS_REGION is not set"
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "AWS_SECRET_ACCESS_KEY is not set"
+    exit 1
+fi
+
+# sui,sui-node
+if [ -z "$BINARIES" ]; then
+    echo "BINARIES is not set"
+    exit 1
+fi
+
+if [ -z "$COMMIT" ]; then
+    echo "COMMIT is not set"
+    exit 1
+fi
+
+if [ -z "$REPO_URL" ]; then
+    echo "REPO_URL is not set"
+    exit 1
+fi
+
+rm -rf /data/*
+
+CARGO_CMD="cargo build --release --target-dir /data"
+
+IFS=',' read -ra BINS <<< "$BINARIES"
+for bin in "${BINS[@]}"; do
+  CARGO_CMD+=" --bin $bin"
+done
+
+echo "Build Command: $CARGO_CMD"
+
+mkdir -p /data/repo
+mkdir -p /data/upload
+
+cd /data && git clone $REPO_URL /data/repo
+cd /data/repo && git checkout $COMMIT
+cd /data/repo && eval "$CARGO_CMD"
+
+IFS=',' read -ra BINS <<< "$BINARIES"
+for bin in "${BINS[@]}"; do
+  CARGO_CMD+=" --bin $bin"
+  mv /data/release/$bin /data/upload/$bin
+  s5cmd --endpoint-url $AWS_ENDPOINT_URL  cp '/data/upload/*' s3://$AWS_BUCKET_NAME/$COMMIT
+done
